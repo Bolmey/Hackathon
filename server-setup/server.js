10f3559a5974b278da60037e4216ff61e597712c
miliberlin <michelle.liebheit@posteo.de>
2021-05-08 08:36:03 +0200

    finished basic server setup, added cors

--- server-setup/server.js
+++ server-setup/server.js
@@ -3,9 +3,11 @@ const socket = require('socket.io');
 const cors = require('cors');
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.use(require('cors')())
+
+const server = app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
 
